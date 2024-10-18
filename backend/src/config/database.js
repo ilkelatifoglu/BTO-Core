257d20ca8a46100bcf0736dcba0a512ac6c70eec
Emre Yazicioglu <emreyaziciogl@gmail.com>
2024-10-18 14:46:51 +0300

    updated database.js to connect using railway url

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_URL,
 });
 
 const query = (text, params) => pool.query(text, params);

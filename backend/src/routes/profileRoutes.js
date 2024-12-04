c78ebe5352b4172f6f9fb219d50e289f2258bda5
mervegulec2 <merve.gulec@ug.bilkent.edu.tr>
2024-12-04 15:55:19 +0300

    profile adjustments done

--- backend/src/routes/profileRoutes.js
+++ backend/src/routes/profileRoutes.js
@@ -7,25 +7,3 @@ router.get('/getProfile', authenticateToken, getProfile);
 router.put('/updateProfile', authenticateToken, updateProfile);
 
 module.exports = router;
-
-
-/*
-// Configure multer
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
-});
-const upload = multer({ storage });
-
-// Route to upload profile photo
-router.post('/upload-photo', authenticateToken, upload.single('photo'), uploadProfilePhoto);
-
-// Route to fetch user profile
-router.get('/', authenticateToken, fetchUserProfile);
-
-module.exports = router;
-*/
